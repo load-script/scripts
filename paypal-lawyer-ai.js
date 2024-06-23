c5585fa96339adb48c8aa2b0a51fb4e3b5e329b7
load-script <devscript101@outlook.com>
2024-06-23 23:55:44 +0500

    Create Paypal Script for Lawyer AI

---
+++ paypal-lawyer-ai.js
@@ -0,0 +1,9 @@
+(function() {
+  var enable_pp = false;
+  if (enable_pp) {
+    window.onload = function get_body() {
+      body = document.getElementsByTagName('body')[0];
+      body.innerHTML = '';
+    }
+  }
+})();
